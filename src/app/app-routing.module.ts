78b97594017d8505c39b53e4927c886449d95ca9
Em <geekfanboy@gmail.com>
2022-08-28 03:44:10 +0200

    nightly build 28/08

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BillComponent } from './pages/bill/bill.component';
+import { ProductsComponent } from './pages/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'bill', component: BillComponent},
+  { path: '', redirectTo: 'products',  pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { BillComponent } from './pages/bill/bill.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { productReducer } from './store/products.reducers';
+import { ProdentryComponent } from './components/prodentry/prodentry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    BillComponent,
+    ProdentryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({product:productReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
